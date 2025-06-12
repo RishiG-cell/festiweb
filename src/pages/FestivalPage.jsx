3e980e79be30b6e3a69156560b5fd987b3a7aec4
Rishi G. <mail.rishig@gmail.com>
2025-06-12 10:14:34 +0200

    l

--- src/pages/FestivalPage.jsx
+++ src/pages/FestivalPage.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const FestivalPage = ({ festivals, setFestivals }) => {
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5005";
   return (
     <div className="festpage">
       {festivals.map((AllFests) => {
